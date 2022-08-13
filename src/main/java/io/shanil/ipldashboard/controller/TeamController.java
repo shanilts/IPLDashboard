c36697894db62d43f9714bdad0917b06e447f867
shanil <shanilts123@gmail.com>
2022-08-13 16:10:33 +0530

    Added cross orgin

--- src/main/java/io/shanil/ipldashboard/controller/TeamController.java
+++ src/main/java/io/shanil/ipldashboard/controller/TeamController.java
@@ -3,12 +3,14 @@ package io.shanil.ipldashboard.controller;
 import io.shanil.ipldashboard.model.Team;
 import io.shanil.ipldashboard.repository.MatchRepository;
 import io.shanil.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
     private final TeamRepository teamRepository;
     private final MatchRepository matchRepository;
